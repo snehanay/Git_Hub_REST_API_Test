d1b534b502283447c8e65467b44d3aa5b0a9571a
Sneha Nayak <sneha.nayak@ellucian.com>
2022-11-17 22:10:09 +0530

    Initial commit

---
+++ src/test/java/APITests.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITests {
+    @Test
+    void test1(){
+       Response response = RestAssured.get("http://10.0.64.223:8082/PlatformSandboxApp/actuator/health");
+
+       System.out.println("Response : "+response.asString());
+       System.out.println("Status Code : "+response.getStatusCode());
+       System.out.println("Body :"+response.getBody().asString());
+       System.out.println("Time :"+response.getTime());
+       System.out.println("Header :"+response.getHeader("content-type"));
+
+       int statusCode = response.getStatusCode();
+       Assert.assertEquals(statusCode,200);
+    }
+}
