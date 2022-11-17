d68a72f0a98c7d75108d78ec28d5718fc836217b
Sneha Nayak <sneha.nayak@ellucian.com>
2022-11-17 23:29:49 +0530

    Commit

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -3,18 +3,30 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class APITests {
     @Test
-    void test1(){
-       Response response = RestAssured.get("http://10.0.64.223:8082/PlatformSandboxApp/actuator/health");
+    void test1() {
+        Response response = RestAssured.get("http://10.0.64.223:8082/PlatformSandboxApp/actuator/health");
 
-       System.out.println("Response : "+response.asString());
-       System.out.println("Status Code : "+response.getStatusCode());
-       System.out.println("Body :"+response.getBody().asString());
-       System.out.println("Time :"+response.getTime());
-       System.out.println("Header :"+response.getHeader("content-type"));
+        System.out.println("Response : " + response.asString());
+        System.out.println("Status Code : " + response.getStatusCode());
+        System.out.println("Body :" + response.getBody().asString());
+        System.out.println("Time :" + response.getTime());
+        System.out.println("Header :" + response.getHeader("content-type"));
 
-       int statusCode = response.getStatusCode();
-       Assert.assertEquals(statusCode,200);
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
     }
+
+    @Test
+    void test2(){
+
+        given().
+                get("http://10.0.64.223:8082/PlatformSandboxApp/actuator/health").
+                then().
+                statusCode(200);
+    }
+
 }
